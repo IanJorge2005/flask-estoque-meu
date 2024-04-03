4e1f7e5db34371fdbe4ae4f10fd3abbbd0cfcbfa
ALUNO <aluno@aluno.ifsp.edu.br>
2024-04-03 20:55:45 -0300

    Aula do Lobatinho<3 03/04/2024

--- src/factory.py
+++ src/factory.py
@@ -4,7 +4,9 @@ import sys
 
 from flask import Flask, render_template
 
-from src.modulos import bootstrap
+from src.modulos import bootstrap, minify, db
+from utils import existe_esquema
+
 
 def create_app(config_filename: str = 'config.dev.json') -> Flask:
     app = Flask(__name__,
@@ -19,15 +21,24 @@ def create_app(config_filename: str = 'config.dev.json') -> Flask:
     try:
         app.config.from_file(config_filename, load=json.load)
     except FileNotFoundError:
-        app.logger.critical('Não existe o arquivo da configuração informado')
+        app.logger.critical("Não existe o arquivo da configuração informado")
         sys.exit(1)
 
     bootstrap.init_app(app)
+    if app.config.get('MINIFY', False):
+        minify.int.app(app)
+    db.init_app(app)
+
+    with app.app_context():
+        if not existe_esquema(app):
+            app.logger.critical("Efetuar a migração/upgrade do banco")
+            sys.exit(1)
+
 
     @app.route('/')
     @app.route('/index')
     def index():
         return render_template('index.jinja',
-                               title='Página Principal')
+                               title="Página Principal")
 
     return app
